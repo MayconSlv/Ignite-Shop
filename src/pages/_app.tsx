30bacea0148a5e2f1384ea0e32dfef4b29b4cba2
Maycon Silva <mayconslv.contato@gmail.com>
2023-03-30 13:38:37 -0300

    Setup inicial da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
+import { globalStyle } from '@/styles/global'
 import type { AppProps } from 'next/app'
+import { Roboto } from '@next/font/google'
 
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+})
+
+globalStyle()
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
