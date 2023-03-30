cbefa0752ed888ff1ee26da9b30c9623febc8d31
Maycon Silva <mayconslv.contato@gmail.com>
2023-03-30 13:50:49 -0300

    Criando header da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import { globalStyle } from '@/styles/global'
 import type { AppProps } from 'next/app'
 import { Roboto } from '@next/font/google'
+import { Container, Header } from '@/styles/pages/app'
+import logoImg from '../assets/logo.svg'
+import Image from 'next/image'
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -10,8 +13,11 @@ const roboto = Roboto({
 globalStyle()
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={roboto.className}>
+    <Container className={roboto.className}>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
       <Component {...pageProps} />
-    </main>
+    </Container>
   )
 }
