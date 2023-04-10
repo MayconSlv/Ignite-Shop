528159f651abe51799fc7b1673508d236b623dd0
Maycon Silva <mayconslv.contato@gmail.com>
2023-04-10 09:38:32 -0300

    Estrutua pagina Success

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,3 +1,20 @@
+import { SuccessContainer, ImageContainer } from '@/styles/pages/success'
+import Link from 'next/link'
+
 export default function Success() {
-  return <h1>parabens</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhull <strong>Maycon Silva</strong>, sua
+        <strong> Camisa tal tal </strong>
+        já está a caminho de casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
